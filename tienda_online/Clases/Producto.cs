985d48a817359567c0925f88d5238abf9c6a8a19
Francis <francisco-bujonok@outlook.com>
2025-08-26 13:45:58 -0300

    commit
    Identifiqué e hice las clases

---
+++ tienda_online/Clases/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+        public int Stock { get; set; }
+
+        public Producto (int id, string nombre, int precio, int stock) 
+        {
+            this.ID = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Stock = stock;
+        }
+    }
+}
